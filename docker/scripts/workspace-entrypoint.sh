035602b4cf5ce84cd01c15b28b036f6fc03cbbf6
Aravind Sairam <aravindsairam1995@gmail.com>
2024-04-24 22:12:45 +0200

    added sources and updated dockerfile

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -12,6 +12,12 @@
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
+echo "source /nano_workspace/install/setup.bash" >> ~/.bashrc
+source /nano_workspace/install/setup.bash
+
+echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
+source /workspaces/isaac_ros-dev/install/setup.bash
+
 sudo apt-get update
 rosdep update
 
