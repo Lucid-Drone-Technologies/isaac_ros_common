e7de1486aa588d776cd6ec1a9658a780b61154ed
Aravind Sairam <aravindsairam1995@gmail.com>
2024-04-26 17:12:56 +0200

    added path to libnvdla.so

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -12,6 +12,8 @@
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/aarch64-linux-gnu/tegra/
+
 echo "source /nano_workspace/install/setup.bash" >> ~/.bashrc
 source /nano_workspace/install/setup.bash
 
