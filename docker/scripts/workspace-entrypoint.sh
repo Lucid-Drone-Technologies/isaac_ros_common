2a855c3172b5d722ec69dc187293e97f6dfab4a7
Aravind Sairam <aravindsairam1995@gmail.com>
2024-05-10 11:01:27 +0200

    isaac in cloud

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -12,11 +12,6 @@
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/aarch64-linux-gnu/tegra/
-
-echo "source /nano_workspace/install/setup.bash" >> ~/.bashrc
-source /nano_workspace/install/setup.bash
-
 echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
 source /workspaces/isaac_ros-dev/install/setup.bash
 
