36237e01e716c6c3d0e5484ccb709874de1c3f55
aravindlucid <asairam@lucidbots.com>
2024-08-12 20:41:10 +0200

    adding workspace sourcing

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -12,6 +12,9 @@
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
+echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
+source /workspaces/isaac_ros-dev/install/setup.bash
+
 # Restart udev daemon
 sudo service udev restart
 
