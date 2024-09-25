fdb86473031967f40683109e673753e4f58ee870
Aravind Sairam <asairam@lucidbots.com>
2024-09-25 12:30:13 +0200

    sourcing local_setup.bash

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -15,6 +15,9 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
 source /workspaces/isaac_ros-dev/install/setup.bash
 
+echo "source /workspaces/isaac_ros-dev/install/local_setup.bash" >> ~/.bashrc
+source /workspaces/isaac_ros-dev/install/local_setup.bash
+
 # Restart udev daemon
 sudo service udev restart
 
